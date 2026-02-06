2ad533c0f584bfc32e841274783371a391f44f83
Insertted <seregasmolnyi48@gmail.com>
2026-02-06 09:57:57 +0300

    added auth, fixed text, fixed code

--- app/script.js
+++ app/script.js
@@ -5,6 +5,10 @@ const typerText = document.getElementById('typer-text');
 
 const loaderFrames = ['/', '-', '\\', '|'];
 
+let isAuth = false;
+let curStep = 'system'; // По умолчанию мы в системе как гость
+const regdata = { user: 'observer012', password: 'pan'}
+
 function getDateTime() {
     const now = new Date();
     const options = { 
@@ -26,7 +30,11 @@ window.onload = async () => {
 }
 
 input.addEventListener('input', () => {
-    typerText.textContent = input.value;
+    if (curStep === 'auth_password') {
+        typerText.textContent = "*".repeat(input.value.length);
+    } else {
+        typerText.textContent = input.value;
+    }
 });
 
 async function showLoader(duration = 1500) {
@@ -64,33 +72,87 @@ async function typeWriter(text, speed = 30) {
 
 input.addEventListener('keydown', async (e) => {
     if (e.key === 'Enter') {
-        const command = input.value.toLowerCase().trim();
-        const rawCommand = input.value;
+        const val = input.value.trim();
+        const raw = input.value;
         
         input.value = '';
         typerText.textContent = '';
 
-        if (command === '') return;
+        if (val === '' && curStep !== 'auth_password') return;
 
+        // Отображение ввода в истории
         const userLine = document.createElement('div');
-        userLine.innerHTML = `<span style="color: #888;">AACS:\\> ${rawCommand}</span>`;
+        userLine.innerHTML = `<span style="color: #888;">AACS:\\> ${curStep === 'auth_password' ? '********' : raw}</span>`;
         history.appendChild(userLine);
         
-        await showLoader(2900); 
+        await showLoader(1000); 
 
-        if (command === 'help') {
-            await typeWriter('Wel▓̡̋́▓͑̃▓̍ͥme to Autom▓̡̋́te# An##nna Co▓͑̃▓̍ͥnicating Se#▓̍ͥice.\\nTerm▓̡̋́▓͑̃▓̍ͥl response to co##ands:\\nLOGS\\nSTATUS\\nCLEAR');
-        } else if (command === 'clear') {
-            history.innerHTML = '';
-        } else if (command === 'status') {
-            await typeWriter('Diagnostic. . .')
-            await showLoader(4000)
-            await typeWriter('Server_connection................OK\\nAntenna_translators..............OK\\nTransformer_substation3102.......OK\\nTransformer_substation1429.......OK\\nMain_transofrmer.................ERROR\\nPower_plant_connection...........ERROR\\nRelay_triangulation..............ERROR')
-            await showLoader(3000)
-            await typeWriter('Cur▓̡̋́▓͑̃ent status: POWER OUTAGES')
+        // ЛОГИКА АВТОРИЗАЦИИ ЧЕРЕЗ КОМАНДУ
+        if (curStep === 'auth_login') {
+            if(val.toLowerCase() === regdata.user) {
+                curStep = 'auth_password';
+                await showLoader(1000);
+                await typeWriter('waiting for password. . .');
+            } else {
+                await typeWriter('Unknown user ID. Connection reset.');
+                curStep = 'system';
+            }
         }
-         else {
-            await typeWriter(`ERROR: Command "${command}" not recognized.`);
+        else if (curStep === 'auth_password') {
+            if (val === regdata.password) {
+                curStep = 'system';
+                const dateStr = getDateTime();
+                isAuth = true;
+                await showLoader(2500);
+                input.blur();
+                await typeWriter('ACCESS GRANTED\\n');
+                await showLoader(2000);
+                history.innerHTML = '';
+                await typeWriter(`Welcome to main Automated Antenna Communication Service.\\nYou're logged in as "Observer 012"\\n${dateStr}\\nYour IP address 127.1.1.0\\nType "help" for command list.`)
+                input.focus();
+            } else {
+                await typeWriter('Invalid password. Access denied.');
+                curStep = 'system';
+            }
+        }
+        // ОБЫЧНЫЙ РЕЖИМ КОМАНД
+        else if (curStep === 'system') {
+            const command = val.toLowerCase();
+
+            if (command === 'login') {
+                if (isAuth) {
+                    await typeWriter('You are already logged in.');
+                } else {
+                    curStep = 'auth_login';
+                    await typeWriter('Enter user ID: ');
+                }
+            } else if (command === 'help') {
+                if (!isAuth) {
+                    await typeWriter('Commands:\\nLOGIN\\nLOGS\\nSTATUS\\nCLEAR');
+                } else {
+                    await typeWriter('Commands:\\nLOGS\\nSTATUS\\nLOGOUT\\nCLEAR');
+                }
+            } else if (command === 'status') {
+                if (!isAuth) {
+                    await typeWriter('Diagnostic. . .');
+                    await showLoader(3500);
+                    await typeWriter('Server_connection................OK\\nAntenna_translators..............OK\\nMain_transformer.................OFF\\nNorth_Line.......................ERROR\\nWest_Line........................ERROR\\nStatus: POWER OUTAGES');
+                } else {
+                    await typeWriter('Diagnostic. . .');
+                    await showLoader(3500);
+                    await typeWriter('Server_connection................OK\\nAntenna_translators..............OK\\nMain_transformer.................OFF\\nNorth_Line.......................ERROR\\nWest_Line........................ERROR\\nAgents_online....................16/152\\nStatus: POWER OUTAGES');
+                }
+            } else if (command === 'clear') {
+                history.innerHTML = '';
+            } else if (command === 'logout') {
+                isAuth = false;
+                await typeWriter('Session terminated.');
+                await showLoader(1500);
+                history.innerHTML = '';
+                window.onload();
+            } else {
+                await typeWriter(`ERROR: Command "${command}" not recognized.`);
+            }
         }
     }
 });
