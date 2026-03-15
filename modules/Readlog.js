f554881bc3106d61ae116616c9934757c0825949
Insertted <seregasmolnyi48@gmail.com>
2026-03-16 01:16:44 +0300

    test fixes for pages

--- modules/Readlog.js
+++ modules/Readlog.js
@@ -1,6 +1,6 @@
 export async function readLogFile(fileName) {
     try {
-        const response = await fetch(`logs/${fileName}`);
+        const response = await fetch(`./logs/${fileName}`);
         if (!response.ok) throw new Error('File not found');
         const text = await response.text();
         return text.replace(/\r?\n/g, '\\n');
